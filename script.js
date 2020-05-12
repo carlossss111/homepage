48fe04c72f2f16d0dc7076d383247e0fad597547
LinuxDaniel <danielrobinson1112@gmail.com>
2020-05-12 14:28:33 +0100

    Added different background images depending on weather conditions. Fixed background image sizing.

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ const getWeatherData = async function(){
     //Constructs the URL by concaternating the main site and the query string.
     const apiKey = "59c1a6efb5ee1aa080e71cdfc6293b6c";
     const units = "metric"
-    const myLocation = "Norwich,uk";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${myLocation}&units=${units}&appid=${apiKey}`;
+    const myLocation = "lat=52.67&lon=1.31";
+    const url = `https://api.openweathermap.org/data/2.5/weather?${myLocation}&units=${units}&appid=${apiKey}`;
     console.log("Connecting to:", url);
 
     //Tries to connect to the url using fetch() inbuilt function. Awaits response then if the response is valid
@@ -41,33 +41,54 @@ const getWeatherData = async function(){
         if (response.ok){
             console.log("Connection successful!")
             const jsonResponse = await response.json();
+            console.log("JSON Response:",jsonResponse);
             return jsonResponse;
         }
     //Error checking for an unreadable response and network connectivity issues, in that order.
         throw new Error("Request Failed, response.ok != true.");
     }
     catch(err){
-        console.log("Weather failed to load.");
+        console.log("Weather failed to load:");
         console.log(err);
     }
 }
 
 //Takes data (response from a GET request) and navigates the openweather.org API.
 //Returns descriptors in order of interest. Boring ones are nearer the bottom.
+//A background image is also changed depending on the condiiton.
 const getWeatherDescriptor = async function(rawData){
     var data = await rawData;
 
-    if(data.weather[0].main === "Snow"){ return "snowy"; }
-    else if (data.weather[0].main === "Thunderstorm"){ return "thunderous"; }
-    else if (data.weather[0].main === "Rain"){ return "rainy"; }
-    else if (data.main.temp >= 30){ return "hot"; }
-    else if (data.main.temp <= 0){ return "freezing"; }
-    else if (data.weather[0].main === "Mist"){ return "misty"; }
-    else if (data.main.humidity >= 70){ return "humid"; }
-    else if (data.wind.speed >= 8){ return "windy"; }
-    else if (data.weather[0].main === "Clouds"){ return "cloudy"; }
-    else if (data.weather[0].main === "Drizzle"){ return "damp"; }
-    else{ return "clear"};
+    if(data.weather[0].main === "Snow"){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/snow.webp')";
+        return "snowy"; }
+    else if (data.weather[0].main === "Thunderstorm"){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/thunderstorm.webp')";
+        return "thunderous"; }
+    else if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle"){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/rain.gif')";
+        return "rainy"; }
+    else if (data.main.temp >= 30){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/hot.jpg')";
+        return "hot"; }
+    else if (data.main.temp <= 0){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/cold.jpg')";
+        return "freezing"; }
+    else if (data.weather[0].main === "Mist"){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/mist.png')";
+        return "misty"; }
+    else if (data.main.humidity >= 70){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/humid.jpg')";
+        return "humid"; }
+    else if (data.wind.speed >= 8){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/wind.jpg')";
+        return "windy"; }
+    else if (data.weather[0].main === "Clouds"){ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/clouds.jpg')"
+        return "cloudy"; }
+    else{ 
+        document.getElementById("backgroundImage").style.backgroundImage = "url('./images/backgrounds/clear.jpg')";
+        return "clear"};
 }
 
 //Prints the weather descriptor and the day of the week to the console and the HTML document.
