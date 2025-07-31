c0686dd7892972b743a3adfae60f8c57ff57be47
Daniel <danielrobinson1112@gmail.com>
2025-07-31 21:09:56 +0100

    Add springboot

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A spring homepage hosted with Apache Tomcat and templated with Java Thymeleaf.
 
+## Running
+Run a webserver on port 8080 with maven:
+```
+mvn spring-boot:run
+```
 
---
+++ src/test/java/com/daniel/homepage/BrowserHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
